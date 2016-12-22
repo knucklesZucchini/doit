367af7f59dc827fd4b8cedadcb75a2c867808771
Mark Krawczuk <mkrawczuk@tw-mbp-mkrawczuk.attlocal.net>
2016-12-21 16:52:39 -0800

    Setting up project

---
+++ Doit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Doit
+//
+//  Created by Mark Krawczuk on 12/21/16.
+//  Copyright © 2016 Mark Krawczuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
